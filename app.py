2a71474a8b90dfccc5c5917f94075ae0fbba5dbf
Gauravtripathi12333 <raajgaurav633@gmail.com>
2025-10-19 18:49:10 +0530

    Add PORT environment variable configuration

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from io import BytesIO
 # -----------------------------
 app = Flask(__name__)
 load_dotenv()
+port = int(os.environ.get("PORT", 5000))
 
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
